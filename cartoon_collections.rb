e94bcf59b3303893e8cb059e06363808ace4b70d
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-26 03:32:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map do |planeteer_calls|
-    print "#{planeteer_calls.capitalize}!"
+    puts "#{planeteer_calls.capitalize}!"
   end
 end
 

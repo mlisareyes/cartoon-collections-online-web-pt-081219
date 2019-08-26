eea6721b414789ff6c199a974e2b3049e2972ff8
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-26 04:25:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,4 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.include?(cheddar)
-
-
 end
